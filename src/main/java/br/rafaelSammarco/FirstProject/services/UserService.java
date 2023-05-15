59f0311cbe5960a8b68709353b9992a649688a26
Rafael Sammarco <rafarhs286@gmail.com>
2023-05-15 08:55:55 -0300

    Exception handling - findById

--- src/main/java/br/rafaelSammarco/FirstProject/services/UserService.java
+++ src/main/java/br/rafaelSammarco/FirstProject/services/UserService.java
@@ -2,6 +2,7 @@ package br.rafaelSammarco.FirstProject.services;
 
 import br.rafaelSammarco.FirstProject.entities.User;
 import br.rafaelSammarco.FirstProject.repositories.UserRepository;
+import br.rafaelSammarco.FirstProject.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
