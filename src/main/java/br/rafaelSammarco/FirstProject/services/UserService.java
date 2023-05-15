0699da23ef93804c681d57b1be42d1572278417d
Rafael Sammarco <rafarhs286@gmail.com>
2023-05-15 09:35:37 -0300

    Exception handling - delete

--- src/main/java/br/rafaelSammarco/FirstProject/services/UserService.java
+++ src/main/java/br/rafaelSammarco/FirstProject/services/UserService.java
@@ -2,8 +2,11 @@ package br.rafaelSammarco.FirstProject.services;
 
 import br.rafaelSammarco.FirstProject.entities.User;
 import br.rafaelSammarco.FirstProject.repositories.UserRepository;
+import br.rafaelSammarco.FirstProject.services.exceptions.DatabaseException;
 import br.rafaelSammarco.FirstProject.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -28,7 +31,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(e);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
